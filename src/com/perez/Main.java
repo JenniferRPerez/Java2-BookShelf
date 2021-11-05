04b7c20f6d360b9393967c09a2a035f4a28b7afd
Austin Timmerman <timmermanaustin20@gmail.com>
2021-11-04 20:34:46 -0500

    Add Book Class

--- src/com/perez/Main.java
+++ src/com/perez/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hi!");
+        Book b4 = new ChildrensBook("Java for Kids", "Your Name", "Your Partner's Name");
     }
 }
